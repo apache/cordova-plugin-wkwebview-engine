a4f3e8848508ea8e12e03e168f6cf398400ede1c
Luca Torella <lucatorella@gmail.com>
2016-09-07 09:35:52 +0200

    CB-11815: (iOS) Fix hard-coded bridge name "cordova"
     This closes #18

--- src/ios/CDVWKWebViewEngine.m
+++ src/ios/CDVWKWebViewEngine.m
@@ -98,7 +98,7 @@
     }
 
     if ([self.viewController conformsToProtocol:@protocol(WKScriptMessageHandler)]) {
-        [wkWebView.configuration.userContentController addScriptMessageHandler:(id < WKScriptMessageHandler >)self.viewController name:@"cordova"];
+        [wkWebView.configuration.userContentController addScriptMessageHandler:(id < WKScriptMessageHandler >)self.viewController name:CDV_BRIDGE_NAME];
     }
 
     [self updateSettings:settings];
