e248743105714d0eddd1d8c68e4157b714064474
Manuel Mtz-Almeida <manu.valladolid@gmail.com>
2017-04-04 16:35:57 +0200

    Adds lightweight XHR rewriter

--- src/ios/wk-plugin.js
+++ src/ios/wk-plugin.js
@@ -9,18 +9,40 @@
     return;
   }
 
-  var stopScrollHandler = window.webkit.messageHandlers.stopScroll;
-  if (!stopScrollHandler) {
-    return;
+  function wkRewriteURL(url) {
+    if (!url) {
+      return url;
+    }
+    if (!url.startsWith("file://")) {
+      return url;
+    }
+    url = url.substr(7); // len("file://) == 7
+    if (url.length > 0 && url[0] === '/') { // ensure the new URL starts with /
+      return url;
+    }
+    return '/' + url;
   }
 
-  if (typeof window.wkRewriteURL === 'undefined') {
-    window.wkRewriteURL = function wkRewriteURL(path) {
-      if (!path) {
-        return path;
-      }
-      return path.replace('file:///', '/').replace('file://', '/');
+  if (window.location.hostname === 'localhost') {
+
+    if (typeof window.wkRewriteURL === 'undefined') {
+      window.wkRewriteURL = wkRewriteURL;
     }
+
+    var xhrPrototype = window.XMLHttpRequest.prototype;
+    var originalOpen = xhrPrototype.open;
+
+    xhrPrototype.open = function _wk_open(method, url) {
+      arguments[1] = wkRewriteURL(url);
+      originalOpen.apply(this, arguments);
+    }
+    console.debug("XHR polyfill injected!");
+  }
+
+  var stopScrollHandler = window.webkit.messageHandlers.stopScroll;
+  if (!stopScrollHandler) {
+    console.error('Can not find stopScroll handler');
+    return;
   }
 
   var stopScrollFunc = null;
