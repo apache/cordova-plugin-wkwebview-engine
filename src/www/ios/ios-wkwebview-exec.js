dd612fbac507631510c8afc20ffba01a53bfa615
Nauzer <mark.naus@gmail.com>
2018-08-22 15:45:39 +0200

    Update package.json version to 1.2.0-dev Add back eslint ignore line

--- src/www/ios/ios-wkwebview-exec.js
+++ src/www/ios/ios-wkwebview-exec.js
@@ -124,7 +124,7 @@ var iOSExec = function () {
 iOSExec.nativeCallback = function (callbackId, status, message, keepCallback, debug) {
     var success = status === 0 || status === 1;
     var args = convertMessageToArgsNativeToJs(message);
-    Promise.resolve(cordova.callbackFromNative(callbackId, success, status, args, keepCallback));
+    Promise.resolve(cordova.callbackFromNative(callbackId, success, status, args, keepCallback)); // eslint-disable-line
 };
 
 // for backwards compatibility
